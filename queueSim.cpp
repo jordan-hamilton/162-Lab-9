9d92ef2af1dcaf6d915fe5ae10e6f3747ebeb0e3
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2018-11-25 14:19:32 -0700

    Add comments, allow 0% chance of add/removal

--- queueSim.cpp
+++ queueSim.cpp
@@ -6,6 +6,20 @@ using std::queue;
 using std::rand;
 using std::unique_ptr;
 
+
+/***********************************************************************************************
+** Description: This method declares a queue of integers, and initializes integers for the
+** number of rounds over which to simulate a buffer, as well as the percentage chance of adding
+** and removing a randomly generated number to that buffer, and the average length of the buffer
+** over the course of the simulation. A menu object prompts the user for the number of rounds
+** and chances of adding or removing values, then a while loop generates a random number between
+** 1 and 1000 to add to the end of the buffer using push if a randomly generated number is less
+** than or equal to the user-provided chance of adding a number to the buffer. Another random
+** number is generated to determine if a number should be removed from the front of the buffer,
+** if the random number is less than or equal to the user-provided chance of removing a number.
+** The average is updated and output along with the entire contents of the buffer after each
+** round of the simulation.
+***********************************************************************************************/
 void simulateQueue() {
 
   queue<int> buffer;
@@ -17,17 +31,16 @@ void simulateQueue() {
 
   rounds = queueMenu->getIntFromPrompt("\nHow many rounds would you like to simulate?", 1, 1000, false);
   addChance = queueMenu->getIntFromPrompt("What should the chance of adding a number to the buffer be?\n\
-(Note: Only enter integers to express a percentage)", 1, 100, false);
+(Note: Only enter integers to express a percentage)", 0, 100, false);
   removeChance = queueMenu->getIntFromPrompt("What should the chance of removing a number from the buffer be?\n\
-(Note: Only enter integers to express a percentage)", 1, 100, false);
+(Note: Only enter integers to express a percentage)", 0, 100, false);
 
-  for (auto i = 0; i < rounds; i++) {
-
-    int numToAdd = generateNumber(1000);
+  for (int i = 0; i < rounds; i++) {
 
     cout << endl << "Round " << i + 1 << ": " << endl;
 
     if (generateNumber(100) <= addChance) {
+      int numToAdd = generateNumber(1000);
       buffer.push(numToAdd);
     }
 
@@ -47,14 +60,33 @@ void simulateQueue() {
 
 }
 
+
+/***********************************************************************************************
+** Description: Takes a constant reference to an integer for the maximum random value to return,
+** then generates and returns a random integer between 1 and that value.
+***********************************************************************************************/
 int generateNumber(const int &max) {
   return rand() % max + 1;
 }
 
+
+/***********************************************************************************************
+** Description: Takes a constant reference to a double indicating the average length of the
+** queue before this method was called, as well as a constant reference to an integer for the
+** current round of the buffer simulation and a constant reference to an integer representing
+** the current length of the buffer. The numbers are then plugged into a formula to return the
+** updated average as a double to assign to the avgLength variable.
+***********************************************************************************************/
 double calcAverageLength(const double &prevRoundAvg, const int &round, const int &length) {
   return  (prevRoundAvg * (round - 1) + length) / round;
 }
 
+
+/***********************************************************************************************
+** Description: Takes a queue of integers by value and if the queue is not empty, a while loop
+** displays the queue's contents, using the front method to display the first value and pop to
+** then remove the integer at the front of the queue.
+***********************************************************************************************/
 void outputQueueInfo(queue<int> queueToPrint) {
 
   if ( !queueToPrint.empty() ) {
